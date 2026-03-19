61df5bb0afb73ae98897a9ca97d29ee2ddb8f5a4
watchtiktok85-svg <watchtiktok85@gmail.com>
2026-03-20 01:14:38 +0300

    Update messages.js

--- public/js/messages.js
+++ public/js/messages.js
@@ -186,7 +186,11 @@ function renderMessage(msg) {
     if (msg.type === 'text') {
         content = `<div class="message-content">${escapeHtml(msg.message)}</div>`;
     } else if (msg.type === 'image') {
-    content = `<img src="${msg.message}" class="message-media" onclick="openImageModal('${msg.message}')">`;
+    const imageUrl = msg.message.startsWith('http') 
+        ? msg.message 
+        : (typeof SERVER_URL !== 'undefined' ? SERVER_URL : '') + msg.message;
+    
+    content = `<img src="${imageUrl}" class="message-media" onclick="openImageModal('${imageUrl}')">`;
 } else if (msg.type === 'audio') {
         const duration = msg.duration || 0;
         const minutes = Math.floor(duration / 60);
