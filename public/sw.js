9aa07121c8f10cbef2c76276d8532b26e2933d5d
watchtiktok85-svg <watchtiktok85@gmail.com>
2026-03-29 14:52:57 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'shariq-v1';
+
+// Файлы для кэширования
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/auth.js',
+  '/js/chats.js',
+  '/js/messages.js',
+  '/js/file.js',
+  '/js/settings.js',
+  '/manifest.json'
+];
+
+// Установка Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Возвращаем из кэша, если есть
+        if (response) {
+          return response;
+        }
+        
+        // Иначе запрашиваем с сети
+        return fetch(event.request).then(response => {
+          // Не кэшируем API запросы
+          if (!event.request.url.includes('/api/')) {
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+          }
+          return response;
+        });
+      })
+  );
+});
